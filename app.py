1eddc222d384ab60c602424aa99fdc9df5b2e323
yugyeong1 <dbrud1032@gmail.com>
2022-12-12 12:11:51 +0900

    스트림릿 기본 템플릿

---
+++ app.py
@@ -0,0 +1,13 @@
+# streamlit 라이브러리를 사용하기 위한 임포트문 작성
+
+import streamlit as st
+
+# 웹 대시보드 프레임워크인, 스트림릿은 main 함수가 있어야 한다.
+
+def main() :
+    st.title('대시보드 개발 테스트')
+    st.title('개발 프로젝트')
+
+if __name__ == '__main__' :
+    main()
+
