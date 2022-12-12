f28caeed5e9ca422cab5415a92cb47abb59f2f35
yugyeong1 <dbrud1032@gmail.com>
2022-12-12 16:52:55 +0900

    웹대시보드 스트릿함수 기본 사용법

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ import streamlit as st
 # 웹 대시보드 프레임워크인, 스트림릿은 main 함수가 있어야 한다.
 
 def main() :
-    st.title('대시보드 개발 테스트')
-    st.title('개발 프로젝트')
+    st.title('웹 대시보드 개발 테스트')
 
 if __name__ == '__main__' :
     main()
